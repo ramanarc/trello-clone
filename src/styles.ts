9b9c3d1732942c3e44222902ea0230df2e0062ec
Anuj Raman <anujramanc@gmail.com>
2023-05-18 11:48:45 +0530

    build styled components

---
+++ src/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const ScreenContainer = styled.div`
+  align-items: flex-start;
+  background-color: #0F172A;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 28px;
+  width: 100%;
+`
+
+export const ColumnContainer = styled.div`
+  width: 300px;
+  background-color: #1E293B;
+  min-height: 52px;
+  border-radius: 12px;
+  padding: 16px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+`
+
+export const ColumnTitle = styled.div`
+  color: #F8FAFC;
+  font-weight: bold;
+  margin-bottom: 12px;
+  font-size: 18px;
+`
+
+export const CardContainer = styled.div`
+  background-color: #334155;
+  color: #F1F5F9;
+  padding: 12px 14px;
+  border-radius: 6px;
+  margin: 16px 0px;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+`
